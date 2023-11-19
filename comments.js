e5023e778c58a6797bdf338e486b33333bee6710
Jesaca Lin <92748940+JesacaLin@users.noreply.github.com>
2023-11-19 17:12:42 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,18 @@
+//create web server
+const express = require('express');
+const app = express();
+//parse incoming request bodies
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+//allow cross-origin requests
+const cors = require('cors');
+app.use(cors());
+//connect to database
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/comments');
+//create schema
+const commentSchema = new mongoose.Schema({
+  text: String,
+
+});
